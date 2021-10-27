7ef0c7ab2e22f70e944b4b23d112cf95e1e12ea5
xdoorn <xander.van.doorn@gmail.com>
2021-10-27 16:58:40 +0200

    Promoted App.xml to PrismApplication and changed MainWindow to Shell

--- Prisma/Prisma/App.xaml.cs
+++ Prisma/Prisma/App.xaml.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿// Using .NET
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.Ioc;
+
+// Using Prism
+using Prism.Unity;
+
 
 namespace Prisma
 {
   /// <summary>
-  /// Interaction logic for App.xaml
+  ///   Interaction logic for App.xaml
+  ///   
+  ///   For PrismApplication <see cref="https://prismlibrary.com/docs/wpf/getting-started.html"/>
   /// </summary>
-  public partial class App : Application
+  public partial class App : PrismApplication
   {
+    protected override Window CreateShell()
+    {
+      var shell = Container.Resolve<Shell>();
+      return shell;
+    }
+
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+      // Nothing to register at the moment.
+    }
   }
 }
